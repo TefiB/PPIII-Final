d491c01c2abeeca0f070e7d50f9c43fdc223e05d
TefiB <109102705+TefiB@users.noreply.github.com>
2022-07-29 22:52:05 -0300

    pantalla usuario
    primera version de pantalla usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoiftsComponent } from './components/logoifts/logoifts.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UserComponent } from './components/user/user.component';
+import { CentroComponent } from './components/centro/centro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    HeaderComponent,
+    LogoiftsComponent,
+    FooterComponent,
+    NavbarComponent,
+    UserComponent,
+    CentroComponent
+    
   ],
   imports: [
     BrowserModule,
